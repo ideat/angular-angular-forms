4c9d4a97ae2ed5b1f617f82ee744b15437d7f23c
freddy <jfreddy.caballero@gmail.com>
2022-02-14 21:45:50 -0400

    Fomularios - Template y Lazyload

---
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -0,0 +1,54 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+interface Persona  {
+  nombre: string,
+  favoritos: Favorito[]
+}
+
+interface Favorito{
+  id: number,
+  nombre: string;
+}
+
+
+@Component({
+  selector: 'app-dinamicos',
+  templateUrl: './dinamicos.component.html',
+  styles: [
+  ]
+})
+export class DinamicosComponent {
+
+  nuevoJuego: string ='';
+
+  persona: Persona = {
+    nombre: 'Freddy',
+    favoritos:[
+      {id:1, nombre:'Metal Gear'},
+      {id:2, nombre: 'DeathStranding'}
+    ]
+  }
+
+  @ViewChild('miFormulario') miFormulario!:NgForm;
+
+  guardar(){
+    console.log(this.miFormulario.controls['nombre']?.errors);
+  }
+
+  eliminar(index:number){
+    this.persona.favoritos.splice(index,1);
+  }
+
+  agregarJuego(){
+    const nuevoFavorito: Favorito={
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push( ...[nuevoFavorito]);
+    this.nuevoJuego = '';
+  }
+
+}
